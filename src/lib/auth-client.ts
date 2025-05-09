9417c49a3efa005dcf2de42ac2079c18a8d7a289
brunojurakic <bruno.jurakic@fer.unizg.hr>
2025-05-10 00:12:31 +0200

    add login page

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,5 +4,12 @@ import { createAuthClient } from "better-auth/react"
 config({ path: ".env" });
 
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL
+  baseURL: process.env.BETTER_AUTH_URL
 })
+
+export const {
+  signIn,
+  signOut,
+  signUp,
+  useSession
+} = authClient;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,14 +3,20 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db/drizzle";
 import { nextCookies } from "better-auth/next-js";
 import { schema } from "@/db/schema";
- 
+
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg",
-        schema: schema
-    }),
-    emailAndPassword: {
-      enabled: true
-    },
-    plugins: [nextCookies()]
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: schema
+  }),
+  emailAndPassword: {
+    enabled: true
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    }
+  },
+  plugins: [nextCookies()]
 });
