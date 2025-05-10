3d773279954c496d5992ba0b7af70da840815909
brunojurakic <bruno.jurakic@fer.unizg.hr>
2025-05-10 21:35:39 +0200

    add recipe to schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp, boolean} from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, boolean, integer} from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: text('id').primaryKey(),
@@ -46,5 +46,59 @@ export const verification = pgTable("verification", {
   updatedAt: timestamp('updated_at')
 });
 
+export const recipe = pgTable("recipe", {
+  id: text('id').primaryKey(),
+  userId: text('user_id').notNull().references(() => user.id, { onDelete: 'cascade' }),
+  title: text('title').notNull(),
+  description: text('description'),
+  servings: integer('servings').notNull(), // how many people
+  preparationTime: integer('preparation_time').notNull(), // in minutes
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull()
+});
+
+export const category = pgTable("category", {
+  id: text('id').primaryKey(),
+  name: text('name').notNull().unique()
+});
+
+export const recipeCategory = pgTable("recipe_category", {
+  recipeId: text('recipe_id').notNull().references(() => recipe.id, { onDelete: 'cascade' }),
+  categoryId: text('category_id').notNull().references(() => category.id, { onDelete: 'cascade' })
+});
+
+export const ingredient = pgTable("ingredient", {
+  id: text('id').primaryKey(),
+  name: text('name').notNull().unique()
+});
+
+export const recipeIngredient = pgTable("recipe_ingredient", {
+  recipeId: text('recipe_id').notNull().references(() => recipe.id, { onDelete: 'cascade' }),
+  ingredientId: text('ingredient_id').notNull().references(() => ingredient.id, { onDelete: 'cascade' }),
+  quantity: text('quantity').notNull(),
+});
+
+export const instruction = pgTable("instruction", {
+  id: text('id').primaryKey(),
+  recipeId: text('recipe_id').notNull().references(() => recipe.id, { onDelete: 'cascade' }),
+  stepNumber: integer('step_number').notNull(),
+  content: text('content').notNull()
+});
+
+export const review = pgTable("review", {
+  id: text('id').primaryKey(),
+  recipeId: text('recipe_id').notNull().references(() => recipe.id, { onDelete: 'cascade' }),
+  userId: text('user_id').notNull().references(() => user.id, { onDelete: 'cascade' }),
+  content: text('content'),
+  rating: integer('rating').notNull(), // 1-5
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull()
+});
+
+export const bookmark = pgTable("bookmark", {
+  userId: text('user_id').notNull().references(() => user.id, { onDelete: 'cascade' }),
+  recipeId: text('recipe_id').notNull().references(() => recipe.id, { onDelete: 'cascade' }),
+  createdAt: timestamp('created_at').notNull()
+});
 
 export const schema = {user, session, account, verification}
