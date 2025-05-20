d560ed9ffe5217d1e09885408513a200831e9e00
brunojurakic <bruno.jurakic@fer.unizg.hr>
2025-05-20 23:33:06 +0200

    Squashed commit of the following:
    commit 4e2869bc48dc34001d20b0ec8c6861583e687c93
    Author: brunojurakic <bruno.jurakic@fer.unizg.hr>
    Date:   Tue May 20 23:32:44 2025 +0200
    
        attempt to fix images
    
    commit ae996292050c55341b0f95af0e14b2d2fae7e68b
    Author: brunojurakic <bruno.jurakic@fer.unizg.hr>
    Date:   Tue May 20 23:13:56 2025 +0200
    
        change remote patterns to new domain
    
    commit bf9284334a439ef87a5a6125414ee12017a6cb1e
    Author: brunojurakic <bruno.jurakic@fer.unizg.hr>
    Date:   Tue May 20 22:59:15 2025 +0200
    
        revert dev
    
    commit 9e1f2163b70cd44b806e174493adf6f1b46dfb65
    Author: brunojurakic <bruno.jurakic@fer.unizg.hr>
    Date:   Tue May 20 22:57:49 2025 +0200
    
        deploy hook test
    
    commit d9f8444cc2689e7584760cd2a3f4e6d7f63a5850
    Author: brunojurakic <bruno.jurakic@fer.unizg.hr>
    Date:   Tue May 20 22:52:53 2025 +0200
    
        neon install

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,16 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+    domains: ['zavrad.vercel.app'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],    formats: ['image/webp', 'image/avif'],    localPatterns: [
+      {
+        pathname: '/uploads/**',
+        search: '',
+      }
+    ],
+    minimumCacheTTL: 60,
+    qualities: [25, 50, 75, 85, 90, 100],
   },
 };
 
