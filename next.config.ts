5665f38731265f7f3e2fa2c2e67e7e24abc3c820
brunojurakic <bruno.jurakic@fer.unizg.hr>
2025-05-21 00:06:20 +0200

    remove image domain

--- next.config.ts
+++ next.config.ts
@@ -21,17 +21,7 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-    ],
-    domains: ['zavrad.vercel.app', 'public.blob.vercel-storage.com'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],    formats: ['image/webp', 'image/avif'],    localPatterns: [
-      {
-        pathname: '/uploads/**',
-        search: '',
-      }
-    ],
-    minimumCacheTTL: 60,
-    qualities: [25, 50, 75, 85, 90, 100],
+    ]
   },
 };
 
